74f7e3a13f7bad2b9b548c7b0295a27d299b7a7c
timothywalker <timothywalker@live.com>
2015-11-23 19:54:48 +0000

    Example 5

---
+++ ex5.rb
@@ -0,0 +1,15 @@
+my_name   = "Tim K. Walker"
+my_age    = 25
+my_height = 72
+my_weight = 210
+my_eyes   = "blue"
+my_teeth  = "mostly white"
+my_hair   = "blond"
+
+puts "Let's talk about #{my_name}."
+puts "He is #{my_height * 12} feet tall."
+puts "He weighs #{my_weight} pounds."
+puts "He's got #{my_hair} hair, and #{my_eyes} eyes. Dang Arian."
+puts "His teeth are usually #{my_teeth}, depending on the pop."
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight}, I get #{my_age + my_height + my_weight}."
